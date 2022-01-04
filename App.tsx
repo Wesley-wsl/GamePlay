0eee6094dc278c143b8f86846ced5fe269784134
Wesley Westelley <wesleywestelley@gmail.com>
2022-01-04 15:23:32 -0300

    :wrench: feat: add fonts and AppLoading

--- App.tsx
+++ App.tsx
@@ -1,5 +1,31 @@
 import { Sigin } from "./src/screens/signin";
+import { useFonts } from "expo-font";
+import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
+import {
+    Rajdhani_500Medium,
+    Rajdhani_700Bold,
+} from "@expo-google-fonts/rajdhani";
+import AppLoading from "expo-app-loading";
+import { StatusBar } from "react-native";
 
 export default function App() {
-    return <Sigin />;
+    const [fontsLoaded] = useFonts({
+        Inter_400Regular,
+        Inter_500Medium,
+        Rajdhani_500Medium,
+        Rajdhani_700Bold,
+    });
+
+    if (!fontsLoaded) return <AppLoading />;
+
+    return (
+        <>
+            <StatusBar
+                barStyle="light-content"
+                translucent
+                backgroundColor="transparent"
+            />
+            <Sigin />
+        </>
+    );
 }
