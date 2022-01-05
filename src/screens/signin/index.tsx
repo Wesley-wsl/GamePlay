9438fe85c9a35d9e45a5311ff64a6f314db2f2f7
Wesley Westelley <wesleywestelley@gmail.com>
2022-01-05 17:52:10 -0300

    :wrench: feat: add navigation through pages

--- src/screens/signin/index.tsx
+++ src/screens/signin/index.tsx
@@ -1,13 +1,20 @@
-import { View, Text, Image, StatusBar } from "react-native";
+import { View, Text, Image } from "react-native";
 
 import { styles } from "./styles";
 import illustrationImg from "../../assets/illustration.png";
 import { ButtonIcon } from "../../components/ButtonIcon";
+import { useNavigation } from "@react-navigation/native";
+import { homeScreenProps } from "../../@types";
 
-export function Sigin() {
+export function SignIn() {
+    const navigation = useNavigation<homeScreenProps>();
+
+    function handleSigIn() {
+        navigation.navigate("Home");
+    }
+
     return (
         <View style={styles.container}>
-            <StatusBar barStyle="light-content" translucent backgroundColor="transparent" />
             <Image
                 source={illustrationImg}
                 style={styles.image}
@@ -16,14 +23,14 @@ export function Sigin() {
 
             <View style={styles.content}>
                 <Text style={styles.title}>
-                    Organize{"\n"}suas jogatinas{"\n"}facilmente
+                    Conecte-se {"\n"}e organize suas jogatinas
                 </Text>
                 <Text style={styles.subtitle}>
                     Crie grupos para jogar seus games {"\n"} favoritos com seus
                     amigos
                 </Text>
 
-                <ButtonIcon title="Entrar com Discord" activeOpacity={0.7}/>                
+                <ButtonIcon onPress={handleSigIn} title="Entrar com Discord" />
             </View>
         </View>
     );
