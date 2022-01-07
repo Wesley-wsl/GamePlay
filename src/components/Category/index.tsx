230764dcdb6c9651a08ed6ccaa702325702f71b2
Wesley Westelley <wesleywestelley@gmail.com>
2022-01-07 18:22:19 -0300

    :wrench: feat: create Category component

---
+++ src/components/Category/index.tsx
@@ -0,0 +1,35 @@
+import { LinearGradient } from "expo-linear-gradient";
+import { Text, View } from "react-native";
+import { RectButton } from "react-native-gesture-handler";
+import { ICategory } from "../../@types";
+import { theme } from "../../global/styles/theme";
+import { styles } from "./styles";
+
+export function Category({
+    title,
+    hasCheckBox = false,
+    icon: Icon,
+    checked = false,
+    ...rest
+}: ICategory) {
+    const { secondary40, secondary50, secondary70, secondary85 } = theme.colors;
+    return (
+        <RectButton {...rest}>
+            <LinearGradient
+                style={styles.container}
+                colors={[secondary50, secondary70]}
+            >
+                <LinearGradient
+                    style={[styles.content, { opacity: checked ? 1 : 0.5 }]}
+                    colors={[checked ? secondary85 : secondary50, secondary40]}
+                >
+                    {hasCheckBox && (
+                        <View style={checked ? styles.checked : styles.check} />
+                    )}
+                    <Icon width={48} height={48} />
+                    <Text style={styles.title}>{title}</Text>
+                </LinearGradient>
+            </LinearGradient>
+        </RectButton>
+    );
+}
