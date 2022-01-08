714c303eec54aa036f629d2cfc5e9b007d675cbb
Wesley Westelley <wesleywestelley@gmail.com>
2022-01-08 16:00:59 -0300

    :wrench: feat: allow that Background have children

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -1,13 +1,17 @@
 import { LinearGradient } from "expo-linear-gradient";
 import { styles } from "./styles";
 import { theme } from "../../global/styles/theme";
+import { IBackground } from "../../@types";
 
-export function Background() {
+
+export function Background({children}: IBackground) {
     const { secondary80, secondary100 } = theme.colors;
     return (
         <LinearGradient
             style={styles.container}
             colors={[secondary80, secondary100]}
-        />
+        >
+            {children}
+        </LinearGradient>
     );
 }
