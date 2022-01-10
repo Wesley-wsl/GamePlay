eabe7b4e191c48edd1c0fb50d03ebda67025f86a
Wesley Westelley <wesleywestelley@gmail.com>
2022-01-10 16:31:57 -0300

    :wrench: feat: add user informations

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,15 +1,17 @@
 import { Text, View } from "react-native";
+import { useAuth } from "../../hooks/auth";
 import { Avatar } from "../Avatar";
 import { styles } from "./styles";
 
 export function Profile() {
+    const { user } = useAuth();
     return (
         <View style={styles.container}>
-            <Avatar urlImage="https://github.com/wesley-wsl.png" />
+            <Avatar urlImage={user.avatar} />
             <View>
                 <View style={styles.user}>
                     <Text style={styles.greeting}>Olá,</Text>
-                    <Text style={styles.username}>Wesley.</Text>
+                    <Text style={styles.username}>{user.firstName}</Text>
                 </View>
                 <Text style={styles.message}>Hoje é dia de vitória.</Text>
             </View>
