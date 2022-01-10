4c525427bb276556124bdbf5b7e05943e1a54086
Wesley Westelley <wesleywestelley@gmail.com>
2022-01-10 16:27:59 -0300

    :wrench: feat: create Profile component

---
+++ src/components/Profile/index.tsx
@@ -0,0 +1,18 @@
+import { Text, View } from "react-native";
+import { Avatar } from "../Avatar";
+import { styles } from "./styles";
+
+export function Profile() {
+    return (
+        <View style={styles.container}>
+            <Avatar urlImage="https://github.com/wesley-wsl.png" />
+            <View>
+                <View style={styles.user}>
+                    <Text style={styles.greeting}>Olá,</Text>
+                    <Text style={styles.username}>Wesley.</Text>
+                </View>
+                <Text style={styles.message}>Hoje é dia de vitória.</Text>
+            </View>
+        </View>
+    );
+}
