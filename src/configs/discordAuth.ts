39f5e879a27ab34719b4832e22362fec17ed8925
Wesley Westelley <wesleywestelley@gmail.com>
2022-01-12 15:45:23 -0300

    :wastebasket: chore: remove discordAuth configs

--- src/configs/discordAuth.ts
+++
@@ -1,7 +0,0 @@
-const REDIRECT_URI = "https://auth.expo.io/@anonymous/GamePlay-0e2599ee-a808-4281-a5b5-9ee39d0635ec";
-const SCOPE = "identify%20email%20connections%20guilds";
-const RESPONSE_TYPE = "token";
-const CLIENT_ID = "929813839304876102";
-const CDN_IMAGE = "https://cdn.discordapp.com";
-
-export { REDIRECT_URI, SCOPE, RESPONSE_TYPE, CLIENT_ID, CDN_IMAGE };
