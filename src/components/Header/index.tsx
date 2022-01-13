795873ce813634482d9e746b9fe95dfd20c20f05
Wesley Westelley <wesleywestelley@gmail.com>
2022-01-13 14:19:21 -0300

    :pencil2: chore: indentation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -10,29 +10,24 @@ import { useNavigation } from "@react-navigation/native";
 
 export function Header({ title, action }: IHeader) {
     const { secondary100, secondary40, heading } = theme.colors;
-    const navigation = useNavigation()
+    const navigation = useNavigation();
 
     function handleGoBack() {
-        navigation.goBack()
+        navigation.goBack();
     }
 
     return (
-        <LinearGradient style={styles.container} colors={[secondary100, secondary40]}>
+        <LinearGradient
+            style={styles.container}
+            colors={[secondary100, secondary40]}
+        >
             <BorderlessButton onPress={handleGoBack}>
                 <Feather name="arrow-left" size={24} color={heading} />
             </BorderlessButton>
 
             <Text style={styles.title}>{title}</Text>
 
-            {
-                action && (
-                    <View>
-                        {
-                            action
-                        }
-                    </View>
-                )
-            }
+            {action ? <View>{action}</View> : <View style={{ width: 24 }} />}
         </LinearGradient>
     );
 }
